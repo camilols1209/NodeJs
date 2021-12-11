2a8ec783eca685e7fad69e34f14ad950c2e07847
camilols1209 <jcsegrera@uninorte.edu.co>
2021-12-10 23:33:18 -0500

    MongoDB

---
+++ node-express/index.js
@@ -0,0 +1,23 @@
+const express = require('express'),http = require('http');
+const morgan = require('morgan');
+const hostname = 'localhost';
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
+
+const port = 3000;
+
+const app = express();
+
+app.use(morgan('dev')); 
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use('/dishes', dishRouter);
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
